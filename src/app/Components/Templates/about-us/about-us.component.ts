1248ba6d459637d059b61b00693df380fdfb439e
Dani Urbano <daniurbanogarcia26@gmail.com>
2023-05-12 16:26:47 +0200

    tt

--- src/app/Components/Templates/about-us/about-us.component.ts
+++ src/app/Components/Templates/about-us/about-us.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { UserService } from 'src/app/Services/user/user.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Contact } from 'src/app/Models/Contact';
+import { CustomerSupportService } from 'src/app/Services/customer-support/customer-support.service';
 
 @Component({
   selector: 'app-about-us',
@@ -8,5 +13,39 @@ import { UserService } from 'src/app/Services/user/user.service';
 })
 export class AboutUsComponent {
 
+  constructor(private CustomerSupportService: CustomerSupportService) {}
+  contactForm: any;
+  messages: any;
+  show: any;
+  alert:any;
+
+  ngOnInit(){
+    this.contactForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', [Validators.required])
+    })
+  }
+
+  onSubmit(){
+    const data = this.contactForm.value;
+    let contact = new Contact(data.email,data.name,data.message);
+    console.log(contact);
+    this.CustomerSupportService.sendForm(contact).subscribe(response => {
+      if (response.code == '200') {
+        this.alert = 'green';    
+      }else{
+        this.alert = 'red';
+      }
+      this.messages = response.messages;
+      console.log(this.messages);
+      this.show ='show';
+  });
+  }
+
+  closeAlert() {
+    this.show = '';
+    this.alert = '';
+  }
 }
 
